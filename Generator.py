6e5aa64ff5d122ba421697f2d09a4c080d50c276
rodziewiczpiotr <80702771+rodziewiczpiotr@users.noreply.github.com>
2021-03-29 22:32:38 +0200

    Add files via upload

---
+++ Generator.py
@@ -0,0 +1,22 @@
+import random
+
+
+class Generator:
+    buffor = []
+    m = 0
+
+    def set_m(self):
+        Generator.m = int(input("Podaj długość bufora: "))
+
+    def signal(self):
+        for i in range(Generator.m):
+            Generator.buffor.append(random.randint(0, 1))
+
+    def print_signal(self):
+            print(Generator.buffor)
+
+
+first = Generator()
+first.set_m()
+first.signal()
+first.print_signal()
