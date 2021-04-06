e12ae490ff84f4ff75397c76a1ac397529687d50
kiemonc <mikolajchmielecki2000@gmail.com>
2021-04-06 23:22:22 +0200

    mała poprawka w koderze

--- Coder.py
+++ Coder.py
@@ -5,12 +5,8 @@ class Coder(Generator):
 
     def code(self):
         for i in range(0, 3*Generator.m, 3):
-            if Generator.buffor[i] == 0:
-                Generator.buffor.insert(i + 1, 0)
-                Generator.buffor.insert(i + 2, 0)
-            else:
-                Generator.buffor.insert(i + 1, 1)
-                Generator.buffor.insert(i + 2, 1)
+            Generator.buffor.insert(i + 1, Generator.buffor[i])
+            Generator.buffor.insert(i + 2, Generator.buffor[i])
 
 
 first = Coder()
